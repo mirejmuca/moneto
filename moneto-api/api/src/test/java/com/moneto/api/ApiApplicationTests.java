7097fe4b35991d4ee59c8578c619ab0ee4613b28
Mirej <mirejmuca05@gmail.com>
2026-04-07 17:00:03 +0200

    initial commit

---
+++ moneto-api/api/src/test/java/com/moneto/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
